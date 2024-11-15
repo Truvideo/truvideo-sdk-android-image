0b280f567b36131e1ff9319e1f754e306db2d87b
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-11-15 19:10:31 +0000

    Update 76.2.5-rc.31

---
+++ image/src/main/java/com/truvideo/sdk/image/model/ImageEnhancementType.kt
@@ -0,0 +1,18 @@
+package com.truvideo.sdk.image.model
+
+enum class ImageEnhancementType {
+    Exposicion,
+    Luminocidad,
+    Luces,
+    Sombras,
+    Contraste,
+    PuntoNegro,
+    Saturation,
+    Vivacidad,
+    Calidez,
+    Tinte,
+//    Nitidez,
+//    Definicion,
+//    ReduccionDeRuido,
+//    Degradado,
+}
---
+++ image/src/main/java/com/truvideo/sdk/image/ui/edit/activities/edit/TruvideoSdkImageEditContract.kt
@@ -0,0 +1,51 @@
+package com.truvideo.sdk.image.ui.edit.activities.edit
+
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class TruvideoSdkImageEditContract : ActivityResultContract<TruvideoSdkImageEditParams, String?>() {
+    companion object {
+        const val PARAMS = "params"
+        const val OUTPUT_PATH = "output_path"
+    }
+
+    override fun createIntent(context: Context, input: TruvideoSdkImageEditParams): Intent {
+        return Intent(context, TruvideoSdkImageEditActivity::class.java).apply {
+            putExtra(PARAMS, input.toJson())
+        }
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String? {
+        return when (resultCode) {
+            RESULT_OK -> {
+                return intent?.getStringExtra(OUTPUT_PATH)
+            }
+
+            else -> null
+        }
+    }
+}
+
+@Serializable
+data class TruvideoSdkImageEditParams(
+    val inputPath: String,
+    val outputPath: String
+) {
+    fun toJson(): String = jsonConfig.encodeToString(this)
+
+    companion object {
+
+        private val jsonConfig = Json {
+            ignoreUnknownKeys = true
+        }
+
+        fun fromJson(json: String): TruvideoSdkImageEditParams {
+            return jsonConfig.decodeFromString(json)
+        }
+    }
+}
